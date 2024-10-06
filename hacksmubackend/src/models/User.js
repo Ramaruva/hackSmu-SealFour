4ed98b994536db833e3b27578f5e6fbdc7f63bf2
Ramaruva <ramaruva54@gmail.com>
2024-10-05 19:31:08 -0500

    added mongoschema

--- hacksmubackend/src/models/User.js
+++ hacksmubackend/src/models/User.js
@@ -1,13 +1,38 @@
-// src/models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  name: { type: String },
-  userId: { type: String, required: true, unique: true }, // From PropelAuth
+  role: { type: String, enum: ['student', 'consultant'], required: true },  // student or doctor
+  profile: {
+    age: Number,
+    gender: String,
+    phone: String,
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      zip: String,
+    },
+    bio: String,
+    specialty: String,  // For consultants (doctors)
+  },
+  createdAt: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
+// Password hash middleware
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
-module.exports = User;
+// Password validation
+UserSchema.methods.comparePassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
