07a8f67cd52ef64a1b7be0bf586074dcded28721
Ramaruva <ramaruva54@gmail.com>
2024-10-05 23:18:23 -0500

    added doctor reg

--- hacksmubackend/src/controllers/authController.js
+++ hacksmubackend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 const propelAuth = require("@propelauth/express");
 const User = require("../models/User");
+const Doctor = require("../models/Doctor");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
@@ -20,18 +21,29 @@ const register = async (req, res) => {
       return res.status(400).json({ msg: "User already exists" });
     }
     const user = await auth.createUser({ name, email, password, role });
-    console.log(user,"hs")
+    console.log(user, "hs");
     const newUser = new User({
       email: email,
       password: password,
       userId: user.userId,
       name: name,
-      role:role
+      role: role,
     });
     await newUser.save();
-    const token = jwt.sign({ userId: newUser._id, role: newUser.role }, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    });
+    if (role == "doctor") {
+      const newDoctor = new Doctor({
+        doctorId: newUser.userId,
+        specialty: req.body?.specialty,
+      });
+      await newDoctor.save();
+    }
+    const token = jwt.sign(
+      { userId: newUser._id, role: newUser.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
     res.status(201).json({ token, role: newUser.role });
   } catch (error) {
     console.log(error);
@@ -39,12 +51,10 @@ const register = async (req, res) => {
   }
 };
 
-
 // Sign In
 const signin = async (req, res) => {
   const { email, password } = req.body;
   try {
-
     const dbUser = await User.findOne({ email: email });
     if (!dbUser) return res.status(400).json({ message: "User not found" });
 
@@ -55,9 +65,9 @@ const signin = async (req, res) => {
     const token = jwt.sign({ userId: dbUser.userId }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
-    res.status(200).json({ token, userId: dbUser.userId });
+    res.status(200).json({ token, userData: dbUser });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ message: "Error signing in", error });
   }
 };
